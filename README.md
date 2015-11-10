276222e031380931bef06c68539cdd2c16b762e4
Kat Marchán <kzm@sykosomatic.org>
2015-11-10 09:02:39 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mona-csv  [![Travis](https://img.shields.io/travis/zkat/mona-csv.svg)]() [![npm version](https://img.shields.io/npm/v/@mona/csv.svg)]() [![license](https://img.shields.io/npm/l/@mona/csv.svg)]()
+
+[mona-csv](https://github.com/zkat/mona-csv) is a simple, [mona](https://github.com/zkat/mona)-based CSV parsing library.
---
+++ src/index.js
@@ -0,0 +1,63 @@
+import {
+  and,
+  between,
+  or,
+  split,
+  splitEnd
+} from '@mona/combinators'
+
+import {
+  label,
+  value
+} from '@mona/core'
+
+import {
+  parse
+} from '@mona/parse'
+
+import {
+  noneOf,
+  string as str,
+  text
+} from '@mona/strings'
+
+/*
+ * CSV Parser
+ *
+ * Based on parser from http://book.realworldhaskell.org/read/using-parsec.html
+ */
+export function parseCSV (text, minimumColumns) {
+  return parse(csv(minimumColumns), text)
+}
+
+export function csv (minimumColumns) {
+  return splitEnd(line(minimumColumns), eol())
+}
+
+function line (minimumColumns) {
+  return split(cell(), str(','), {min: minimumColumns})
+}
+
+function cell () {
+  return or(quotedCell(), text(noneOf(',\n\r')))
+}
+
+function quotedCell () {
+  return between(str('"'),
+                 label(str('"'), 'closing quote'),
+                 text(quotedChar()))
+}
+
+function quotedChar () {
+  return or(noneOf('"'),
+            and(str('""'),
+                value('"')))
+}
+
+function eol () {
+  return or(str('\n\r'),
+            str('\r\n'),
+            str('\n'),
+            str('\r'),
+            'end of line')
+}
